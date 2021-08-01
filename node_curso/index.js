f3be87a77032153d22289154aa4b55e14be9316d
Renato Oliveira <renato.printf@gmail.com>
2021-07-31 21:18:06 -0300

    Curso em andamento

---
+++ node_curso/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const userRoute = require('./routers/userRoute')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+userRoute(app)
+
+app.get('/', (req, res) => res.send('Olá mundo pelo express!'))
+
+app.listen(port, () => console.log('Api rodando na porta 3000'))
